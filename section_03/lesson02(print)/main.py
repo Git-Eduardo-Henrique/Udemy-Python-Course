73379629a96d9ad40892bb99faeba7d43bb7924c
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-03 01:44:33 -0300

    lesson02

---
+++ section_03/lesson02(print)/main.py
@@ -0,0 +1,5 @@
+# sep="" | cria um novo separador para o conteudo do print
+# end="" | muda o como a linha deve terminar \n para quebrar
+
+print("hello world", 123, sep="-", end=" -> ")
+print("line 2")
