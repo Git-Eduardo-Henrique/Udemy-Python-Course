a879c17c1d4bbd5978f69d9bd432957ae09b9f6e
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-09 01:08:57 -0300

    lesson 04

---
+++ section_03/lesson04(int_float)/main.py
@@ -0,0 +1,6 @@
+print(0, -11, 23) # numero inteiros ( int )
+
+print(10.11, 29.3)  # numeros decimais ( float )
+
+# a classe ( type ) mostra qual o tipo de um valor, como: str, int, boolean
+print("tipo de 1:", type(1), "| tipo de 1.23:", type(1.23))
