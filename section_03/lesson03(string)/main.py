8d20d69584920e9c753c91a76005893e9cfe6bf9
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-09 00:48:46 -0300

    lesson 03

---
+++ section_03/lesson03(string)/main.py
@@ -0,0 +1,12 @@
+# o python imterpreta automaticamente como int (numero inteiro)
+print(1234)
+
+# interpreta como str ( texto, string )
+print('sus')
+print("sus")
+
+print("\"sou sus\"")  # com ( \ ) o python ignora a sintax e exibe "sou sus"
+
+print(r"\"sou sus\"")  # com ( \ ) o python ignora a sintax e exibe "sou sus" e com ( r ) exibe os \
+
+print('"sou sus"') # com aspas diferentes o python so interpreta a primeira aspas e exibe o resto como string
