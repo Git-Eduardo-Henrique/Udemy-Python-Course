6abeca5f7db8ab1ee99218187f20777dfaaf2420
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-15 01:46:26 -0300

    lesson 05

---
+++ section_03/lesson05(boolean)/main.py
@@ -0,0 +1,7 @@
+# == serve para checar se um valor é igual a outro. Caso seja ele é True, se não é False
+# True e False são do tipo Boolean ( bool )
+checar1 = 11 == 11
+checar2 = 11 == 12
+
+print(checar1, checar2)
+print(type(checar1), type(checar2))
