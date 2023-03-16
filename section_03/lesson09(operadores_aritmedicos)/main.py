d017801bc26661af39cc8f15c5fedc0c53248fc7
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-16 01:05:20 -0300

    lesson 09

---
+++ section_03/lesson09(operadores_aritmedicos)/main.py
@@ -0,0 +1,20 @@
+adicao = 2 + 2
+print(f"adicao resultado : {adicao}")
+
+subtracao = 4 - 2
+print(f"subtracao resultado : {subtracao}")
+
+multiplicacao = 2 * 8
+print(f"multiplicacao resultado : {multiplicacao}")
+
+divisao = 24 / 5
+print(f"divisao resultado : {divisao}")
+
+divisao_inteira = 24 // 5
+print(f"divisao inteira resultado : {divisao_inteira}")
+
+exponenciacao = 2 ** 4
+print(f"exponenciacao resultado : {exponenciacao}")
+
+resto_divisao = 55 % 2
+print(f"resto da divisao resultado : {resto_divisao}")
