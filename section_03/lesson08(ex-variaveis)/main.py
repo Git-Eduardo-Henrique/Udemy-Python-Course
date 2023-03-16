7cf68afc4fb187d272db2c095425346140c2ec02
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-16 00:48:47 -0300

    lesson 08

---
+++ section_03/lesson08(ex-variaveis)/main.py
@@ -0,0 +1,15 @@
+nome = "Eduardo"
+sobrenome = "Henrique"
+idade = 18
+nascimento = "16-09-2004"
+maior_veri = idade >= 18
+altura = 1.77
+
+
+
+print(f"nome: {nome}")
+print(f"sobrenome: {sobrenome}")
+print(f"idade: {idade}")
+print(f"ano de nascimento: {nascimento}")
+print(f"é de maior?: {maior_veri}")
+print(f"altura em metros: {altura}")
