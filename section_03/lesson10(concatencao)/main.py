e89fc92074b25278e5fbd1f9b5037d1da430cd29
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-16 01:12:01 -0300

    lesson 10

---
+++ section_03/lesson10(concatencao)/main.py
@@ -0,0 +1,8 @@
+# + com strings realiza a concatenacao que nada mais é do que juntar as strings
+nome = "Eduardo" + " " + "Henrique"
+
+# * com strings ira repetir a string em um determinado numero de vezes
+sus = ("sus" + " ") * 12
+
+print(nome)
+print(sus)
