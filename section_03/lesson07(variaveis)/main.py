eb55e4eab99d578a86eefa884c6a021e2fcb8650
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-16 00:40:55 -0300

    lesson 07

---
+++ section_03/lesson07(variaveis)/main.py
@@ -0,0 +1,8 @@
+nome = "Eduardo"
+idade = 18
+
+# >= serve para verificar se un valor é maior ou igual a outro
+idade_veri = idade >= 18
+
+print(f"Seu nome é {nome} e voce tem {idade} anos")
+print(idade_veri)
