fe8c7f27bdf67c624630c4d6c0a5841bbf875430
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-20 00:54:07 -0300

    lesson 19

---
+++ section_03/lesson19(operadores_racionais)/main.py
@@ -0,0 +1,17 @@
+"""
+> maior que >= maior ou igual a
+< menor que <= menor ou igual a
+== igual a
+!= diferente de
+"""
+
+maior = 2 > 2
+maior_igual = 2 >= 2
+menor = 2 < 2
+menor_igual = 2 <= 2
+igual = "sus" == "amongus"
+diferente = 10 != 11
+
+print(f"{maior=}, {maior_igual=}")
+print(f"{menor=}, {menor_igual=}")
+print(f"{igual=}, {diferente=}")
