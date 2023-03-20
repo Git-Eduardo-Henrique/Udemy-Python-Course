02482cf2165ac614229d5853f095df34d4fb9f69
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-20 00:10:29 -0300

    lesson 15

---
+++ section_03/lesson15(input)/main.py
@@ -0,0 +1,8 @@
+# input permite o usuario passar um valor para a variavel no terminal
+n1 = input("digite um numero: ")
+n2 = input("digite outro numero: ")
+
+n1_int = int(n1)
+n2_int = int(n2)
+
+print(f"{n1_int} + {n2_int} = {n1_int + n2_int}")
