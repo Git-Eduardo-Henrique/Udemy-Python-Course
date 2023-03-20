4272be06464a985943ef41aa00c5b53ab4244a87
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-20 00:23:26 -0300

    lesson 16

---
+++ section_03/lesson16(if_elif_else)/main.py
@@ -0,0 +1,11 @@
+confirma = input("deseja ver algo incrivel? (sim | nao): ")
+
+if confirma == "sim":
+    print("SUS")
+elif confirma == "nao":
+    print("que pena :(")
+else:
+    print("DIGITE SIM OU NÃO")
+
+print("encerrando...")
+
