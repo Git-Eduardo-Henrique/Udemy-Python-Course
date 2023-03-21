46f7b8bf3af1631314c803a1e0bac46f5bee31d1
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-21 00:57:15 -0300

    lesson 23

--- section_03/lesson23(not)/main.py
+++ section_03/lesson23(not)/main.py
@@ -4,7 +4,7 @@ print("Cofre de segurança maxima (confia)")
 print("=" * 50)
 entrar = input("[e] para entrar: ")
 print("=" * 50)
-# or executa o codigo caso alguma condição for verdadeira
+
 if entrar == "E" or entrar == "e":
 
     senha_entrada1 = input("primeira senha: ")
@@ -14,10 +14,15 @@ if entrar == "E" or entrar == "e":
     senha1 = "sus"
     senha2 = "amongus"
 
-    if senha_entrada1 == senha1 and senha_entrada2 == senha2:
-        print("pou 2 foi desbloqueado!!!!")
+    # not valida se algo é falso como por exemplo string vazia
+    if not senha_entrada1 or not senha_entrada2:
+        print("voce não digitou algo!!!")
     else:
-        print("acesso negado")
+        if senha_entrada1 == senha1 and senha_entrada2 == senha2:
+            print("pou 2 foi desbloqueado!!!!")
+        else:
+            print("acesso negado")
+
 else:
     print("que pena")
 
