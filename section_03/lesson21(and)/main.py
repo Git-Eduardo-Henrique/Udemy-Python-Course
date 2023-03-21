2fc2d2f6133338b883d17c5caa28dbdc9e071abc
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-03-21 00:33:55 -0300

    lesson 21

---
+++ section_03/lesson21(and)/main.py
@@ -0,0 +1,12 @@
+print("Cofre de segurança maxima (confia)")
+senha_entrada1 = input("primeira senha: ")
+senha_entrada2 = input("segunda senha: ")
+
+senha1 = "sus"
+senha2 = "amongus"
+
+# and valida duas condições e se as duas forem verdadeiras ele executa o codigo
+if senha_entrada1 == senha1 and senha_entrada2 == senha2:
+    print("pou 2 foi desbloqueado!!!!")
+else:
+    print("acesso negado")
