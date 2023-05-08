4e0dd939bb529548fa43e1016bd5199758d98ba5
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-07 23:08:37 -0300

    aula27

---
+++ secao-03(iniciando)/aula27(len_fatiamento_strings))/main.py
@@ -0,0 +1,9 @@
+"""
+[I:F:P] - I = inicio | F = final | P = passo
+len = conta o tamanho de uma string e outros tipos
+"""
+var = "Eduardo Henrique"
+print(f"o nome '{var}' tem {len(var)} caracteres")
+print(f"nome de traz para frente: {var[::-1]}")
+print(f"primeiro nome: {var[:7]}")
+print(f"ultimo nome: {var[8:]}")
