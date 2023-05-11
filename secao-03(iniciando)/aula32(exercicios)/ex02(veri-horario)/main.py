7d6a4ca7939344c0de294f38c2dbc940832a5f7d
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-11 00:35:40 -0300

    aula32-ex02-v2

--- secao-03(iniciando)/aula32(exercicios)/ex02(veri-horario)/main.py
+++ secao-03(iniciando)/aula32(exercicios)/ex02(veri-horario)/main.py
@@ -1,14 +1,18 @@
 print("=-=" * 20)
-hora = int(input("digite a hora em numero inteiro: "))
+hora = input("digite a hora em numero inteiro: ")
 print("=-=" * 20)
+try:
+    hora_int = int(hora)
+    if hora_int >= 0 and hora_int <= 11:
+        print(f"são {hora_int} horas, Bom dia!")
+    elif hora_int >= 12 and hora_int <= 17:
+        print(f"são {hora_int} horas, Boa Tarde!")
+    elif hora_int >= 18 and hora_int <= 23:
+        print(f"são {hora_int} horas, Boa noite!") 
+    else:
+        print("HORARIO INVALIDO!!!")
 
-if hora >= 0 and hora <= 11:
-    print(f"são {hora} horas, Bom dia!")
-elif hora >= 12 and hora <= 17:
-    print(f"são {hora} horas, Boa Tarde!")
-elif hora >= 18 and hora <= 23:
-    print(f"são {hora} horas, Boa noite!") 
-else:
-    print("HORARIO INVALIDO!!!")
+except:
+    print("HORARIO INVALIDO!!!!")
 
 print("=-=" * 20)
