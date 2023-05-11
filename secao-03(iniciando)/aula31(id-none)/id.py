ff4904dccccb3d9a95e87cee7e7de1921fa382df
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-10 23:49:28 -0300

    aula31-none

--- secao-03(iniciando)/aula31(id-none)/id.py
+++ secao-03(iniciando)/aula31(id-none)/id.py
@@ -2,5 +2,6 @@ var1 = "sus"
 var2 = "sus"
 var3 = "outro"
 
+# id = é endereço da variavel na memoria do pc
 print(f"variaveis com o mesmo valor possuem o mesmo id: var1({id(var1)}) | var2 ({id(var2)})")
 print(f"var3 = {id(var3)}")
---
+++ secao-03(iniciando)/aula31(id-none)/none.py
@@ -0,0 +1,14 @@
+cond = False
+# None = variavel nula / sem tipo ou valor
+passou = None
+
+if cond:
+    passou = True
+
+# is None = verifica se é None
+if passou is None:
+    print("não passou do if")
+
+# is not None = verifica se não é None
+if passou is not None:
+    print("passou do if")
