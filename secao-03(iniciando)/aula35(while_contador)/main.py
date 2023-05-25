e69a0b590ee137fd51d8e5865463e95dca11993d
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-25 01:36:03 -0300

    secao-03-aula-35

---
+++ secao-03(iniciando)/aula35(while_contador)/main.py
@@ -0,0 +1,13 @@
+cont = 0
+
+print(30 * "=-=")
+print("iniciando contador...")
+print(30 * "=-=")
+
+while cont < 10:
+    cont += 1
+    print(f"contador em: {cont}")
+
+print(30 * "=-=")
+print("fim da contagem")
+print(30 * "=-=")
