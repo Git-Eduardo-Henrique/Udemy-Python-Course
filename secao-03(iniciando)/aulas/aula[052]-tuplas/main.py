aa08e46d04a24d7a24ca95cf52d4765b091afcdd
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-12 14:53:32 -0300

    secao-03-aula[052]

---
+++ secao-03(iniciando)/aulas/aula[052]-tuplas/main.py
@@ -0,0 +1,12 @@
+# tupla é uma lista imutavel | não pode modificar os arquivos
+tupla = ("Python", "Among", "Us")
+print(30 * "\033[32m=-=\033[m", "\033[m")
+print(f"tupla: {tupla}")
+print(30 * "\033[32m=-=\033[m", "\033[m")
+
+# tuple() transforma valores em tuplas
+lista = ["Python", "Udemy", "Course"]
+new_tupla = tuple(lista)
+print(f"lista transformada tupla: {new_tupla}")
+print(30 * "\033[32m=-=\033[m", "\033[m")
+
