a74ca8d7d23b9f2337f8c190890932195455b1ba
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-13 18:50:50 -0300

    secao[03]-aula[060]

---
+++ section[03]-init/aulas/aula[060]-if-else-in-line/main.py
@@ -0,0 +1,6 @@
+print(30 * "\033[32m=-=\033[m", "\033[m")
+num = int(input("digite um numero: "))
+print(30 * "\033[32m=-=\033[m", "\033[m")
+
+print("Valor menor do que 10" if num < 10 else "Valor maior que 10")
+print(30 * "\033[32m=-=\033[m", "\033[m")
