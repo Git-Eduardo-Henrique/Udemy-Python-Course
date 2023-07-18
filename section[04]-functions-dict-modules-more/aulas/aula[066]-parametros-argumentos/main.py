5f586e413a51af8e4227cdbacae05ce6ed20857d
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-17 22:30:19 -0300

    secao[04]-aula[066]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[066]-parametros-argumentos/main.py
@@ -0,0 +1,11 @@
+# criar função
+# x - y = parametros
+def soma(x=0, y=0):
+    print(f"{x} + {y} = {x + y}")
+
+# os numeros depois do x e y são argumentos
+print(30 * "\033[32m=-=", "\033[m")
+soma(9, 5)
+soma(x=92, y=212)
+soma(y=3, x=112)
+print(30 * "\033[32m=-=", "\033[m")
