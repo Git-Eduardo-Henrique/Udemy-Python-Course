8b5bdf093560c48af2b03ae592980fb2cbff2c26
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-18 20:53:44 -0300

    secao[04]-aula[070]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[070]-return/main.py
@@ -0,0 +1,10 @@
+def soma(x=0, y=0):
+    # retorna um valor para uma variavel
+    return x + y
+
+
+valor = soma(2, 2)
+
+print(30 * "\033[35m=-=", "\033[m")
+print(f"a soma é: {valor}")
+print(30 * "\033[35m=-=", "\033[m")
