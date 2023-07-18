cd99a7469228dd8f6706032ef857b8db1096e413
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-17 23:02:55 -0300

    secao[04]-aula[068]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[068]-escopo-global/main.py
@@ -0,0 +1,18 @@
+valor = 1
+
+def func1():
+    valor = 11
+    print(f"o valor da funcao 1: {valor}")
+
+def func2():
+    # assim ele irá utilizar a variavel valor do escopo global
+    global valor
+    valor = 12
+    print(f"utilizando o valor normal na função 2: {valor}")
+
+print(30 * "\033[34m=-=", "\033[m")
+
+print(f"valor normal: {valor}")
+func1()
+func2()
+print(30 * "\033[34m=-=", "\033[m")
