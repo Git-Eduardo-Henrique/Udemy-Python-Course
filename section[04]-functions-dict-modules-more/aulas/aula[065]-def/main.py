eb0f661d6e111deb76c9d34a4cc6871c2e48ec68
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-17 22:12:31 -0300

    secao[04]-aula[065]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[065]-def/main.py
@@ -0,0 +1,9 @@
+# criar função
+def soma(x=0, y=0):
+    print(f"{x} + {y} = {x + y}")
+
+print(30 * "\033[31m=-=", "\033[m")
+soma(9, 5)
+soma(10, 2)
+soma(3, 1)
+print(30 * "\033[31m=-=", "\033[m")
