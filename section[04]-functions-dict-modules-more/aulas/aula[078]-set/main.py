4716b4d9b56b1586646859318e56d447a8452995
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-26 22:51:36 -0300

    secao[04]-aula[078]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[078]-set/main.py
@@ -0,0 +1,7 @@
+set1 = set()
+set2 = {1, 2, 3, 4}
+
+print(30 * "\033[32m=-=", "\033[m")
+print(f"o set: {set1} | {type(set1)}")
+print(f"set com valores: {set2} | {type(set2)}")
+print(30 * "\033[32m=-=", "\033[m")
