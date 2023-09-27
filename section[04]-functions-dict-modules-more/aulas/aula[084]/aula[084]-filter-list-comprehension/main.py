33870b388544153045049e14a4ae2f561b20ff43
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-27 14:10:22 -0300

    secao[04]-aula[084]-filter-list-comprehension

---
+++ section[04]-functions-dict-modules-more/aulas/aula[084]/aula[084]-filter-list-comprehension/main.py
@@ -0,0 +1,6 @@
+# só adiciona o numero se for menor ou igual a 5
+lista = [num for num in range(10) if num <= 5]
+
+print(30 * "\033[36m=-=", "\033[m")
+print(f"lista de numeros: \033[36m{lista}\033[m")
+print(30 * "\033[36m=-=", "\033[m")
