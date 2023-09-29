4399bcfc252573de0916a7cdfa63b877f74e5b4d
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-29 14:01:55 -0300

    secao[04]-aula[090]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[090]-iter/main.py
@@ -0,0 +1,12 @@
+# funciona parecido com for porem 100x pior
+lista = ["Eu", "Amo", "Python"]
+iterador = iter(lista)
+
+print(30 * "\033[36m=-=", "\033[m")
+
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+
+print(30 * "\033[36m=-=", "\033[m")
+print(next(iterador))
