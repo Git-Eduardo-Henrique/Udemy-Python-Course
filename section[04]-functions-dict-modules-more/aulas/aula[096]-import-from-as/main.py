758415cc26ce620c50bfff416e0bfdcadb3c7ab9
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-03 14:50:31 -0300

    secao[04]-aula[096]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[096]-import-from-as/main.py
@@ -0,0 +1,7 @@
+# as = da um apilido a um modulo e função
+from sys import exit as ex, platform as plat
+
+print(30 * "\033[31m=-=", "\033[m")
+print(f"seu pc esta na plataforma: \033[31m{plat}\033[m")
+print(30 * "\033[31m=-=", "\033[m")
+ex()
