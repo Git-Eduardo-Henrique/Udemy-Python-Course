556cf47cdf371883f380e9e2973b10e19eaf098b
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-03 15:11:27 -0300

    secao[04]-aula[097]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[097]/aula[097]-modularizacao/main.py
@@ -0,0 +1,6 @@
+import modulo1
+
+# __name__ = mostra o nome do modulo
+# o primeiro modulo iniciado se chama: __main__
+print(f"este modulo se chama: \033[32m{__name__}\033[m")
+print(30 * "\033[32m=-=", "\033[m")
---
+++ section[04]-functions-dict-modules-more/aulas/aula[097]/aula[097]-modularizacao/modulo1.py
@@ -0,0 +1,2 @@
+print(30 * "\033[32m=-=", "\033[m")
+print(f"este modulo se chama: \033[32m{__name__}\033[m")
