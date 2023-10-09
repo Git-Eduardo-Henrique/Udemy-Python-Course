9eae42732774fedbb1529c7dffe3e2b976308554
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-09 15:07:40 -0300

    section[04]-aula[115]

---
+++ section[04]-functions-dict-modules-more/aulas/aula[115]-venv/main.py
@@ -0,0 +1,9 @@
+# comando para criar ambiente virtual
+# python -m venv nome do ambiente (normalmente venv)
+
+# ativar ambiente virtual
+# 1 - ir na pasta antes do venv no terminal
+# 2 - .\venv\Scripts\activate
+
+# desativar ambiente virtual
+# deactivate
